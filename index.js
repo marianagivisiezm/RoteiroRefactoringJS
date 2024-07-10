1548b90b2ca4e7df4ab5f784feeaeb2c2400d950
MarianaGivisiez <marianagivisiezm@gmail.com>
2024-07-09 21:44:42 -0300

    Update index.js
    Replace Temp with Query

--- index.js
+++ index.js
@@ -25,6 +25,11 @@ function gerarFaturaStr(fatura, pecas) {
     return total;
   }  
 
+  // função query
+  function getPeca(apresentacao) {
+    return pecas[apresentacao.id];
+  }  
+
   let totalFatura = 0;
   let creditos = 0;
   let faturaStr = `Fatura ${fatura.cliente}\n`;
@@ -33,16 +38,15 @@ function gerarFaturaStr(fatura, pecas) {
                           minimumFractionDigits: 2 }).format;
 
   for (let apre of fatura.apresentacoes) {
-    const peca = pecas[apre.id];
-
-    let total = calcularTotalApresentacao(apre, peca);
+    //const peca = getPeca(apre);
+    let total = calcularTotalApresentacao(apre, getPeca(apre));
     // créditos para próximas contratações
     creditos += Math.max(apre.audiencia - 30, 0);
-    if (peca.tipo === "comedia") 
+    if (getPeca(apre).tipo === "comedia") 
        creditos += Math.floor(apre.audiencia / 5);
 
     // mais uma linha da fatura
-    faturaStr += `  ${peca.nome}: ${formato(total/100)} (${apre.audiencia} assentos)\n`;
+    faturaStr += `  ${getPeca(apre).nome}: ${formato(total/100)} (${apre.audiencia} assentos)\n`;
     totalFatura += total;
   }
 
